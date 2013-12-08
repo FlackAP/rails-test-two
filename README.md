3ab732044a1a8bbf5adbbe7bf66cbe8682538412
FlackAP <flack.ap@gmail.com>
2013-12-08 14:32:30 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# More Ruby on Rails Fun!
+
+Ruby version:	2.0.0 (x86_64-darwin12.4.1)
+
+This is the second repository created while I learn Ruby on Rails via the [*Ruby on Rails Tutorial*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/). It will follow Chapter 3, which focuses on testing and creating static pages.
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondApp::Application.config.secret_key_base = 'c0991ff9d4ef737a60ca720dfeabe305e93f804c552eb301ba191f150426728d911c0097e395d4facf5c9484c9ebccd330470ae4f1aafe8262f45d64b08b5d1e'
