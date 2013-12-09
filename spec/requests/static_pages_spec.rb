f9e857d4e1ad29bcd05925c3504f67f469baa24f
FlackAP <flack.ap@gmail.com>
2013-12-09 16:12:52 -0500

    passing tests for page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,7 +37,7 @@ describe "Static pages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
