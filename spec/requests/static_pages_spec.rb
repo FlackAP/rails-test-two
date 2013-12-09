3c23ac3d4498856141e389b8ef502432a3f6d5b6
FlackAP <flack.ap@gmail.com>
2013-12-09 16:05:46 -0500

    three failing tests for page titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,26 +2,42 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	describe "Home page" do
+  describe "Home page" do
 
-    	it "should have the content 'Second App'" do
-      		visit '/static_pages/home'
-      		expect(page).to have_content('Second App')
-    	end
-  	end
-
-  	describe "Help page" do
-
-    	it "should have the content 'Help'" do
-      		visit '/static_pages/help'
-      		expect(page).to have_content('Help!')
-    	end
-  	end
-
-    describe "About page" do
-      it "should have the content 'About This Site'" do
-        visit '/static_pages/about'
-        expect(page).to have_content ('About This Site')
-      end
+    it "should have the content 'Second App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Second App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About This Site'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About This Site')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
 end
