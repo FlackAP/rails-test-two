267f8529293320aef048ed5d486254f3de47198b
FlackAP <flack.ap@gmail.com>
2013-12-23 15:44:59 -0500

    updated regex for password validation and left comments for the previous inferior regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase }
+	before_save { email.downcase! }
 	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	# the next line is a reg ex that tests for email validity but allows multiple dots in the address
+	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	# here is an improved reg ex that only allows a single dot in the address
+	VAILD_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true,
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
